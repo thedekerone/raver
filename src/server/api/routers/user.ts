80ce3e6f660552517701dcd61a008ae9c5b122fc
Luis Mauricio Fow Aranibar <mauricio.fow@coolbet.com>
2023-10-22 19:13:36 +0300

    5 test husky

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -4,6 +4,10 @@ import {
   protectedProcedure,
 } from "~/server/api/trpc";
 
+
+
+
+
 export const usersRouter = createTRPCRouter({
   isOwner: protectedProcedure.input(z.string()).query(async ({ctx, input})=>{
     const results =  await ctx.db.user.findFirst({
