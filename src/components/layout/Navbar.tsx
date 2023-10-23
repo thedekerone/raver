f943ac713446816a221a9e575aebcebd7a7c35ec
jorge <jorge.aranibar26@gmail.com>
2023-10-23 01:18:31 -0500

    modal login created

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,44 +1,61 @@
 import {
-    NavigationMenu,
-    NavigationMenuList,
-    NavigationMenuItem,
-    NavigationMenuLink,
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuLink,
 } from "@radix-ui/react-navigation-menu";
 import { signOut, signIn, useSession } from "next-auth/react";
 import { Button } from "../ui/button";
 import { navigationMenuTriggerStyle } from "../ui/navigation-menu";
 import Image from "next/image";
 import Link from "next/link";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import LoginModal from "../LoginModal";
 
 export function Navbar() {
-    const { data: session } = useSession()
+  const { data: session } = useSession();
 
-    return (
-        <div className="container mx-auto flex justify-between items-center p-4">
-            <Link href={"/"}>
-                <div className="flex items-center justify-between">
-                    <Image width="50" height="50" src="/WhatsApp Image 2023-10-15 at 15.01.46.png" alt="logo" />{" "}
-                </div>
-            </Link>
-            <NavigationMenu>
-                <NavigationMenuList className="flex items-center">
-                    <NavigationMenuItem>
-                        <Link href="/admin/events/create" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                create event
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
-
-                    <NavigationMenuItem>
-                        {session ? (
-                            <Button onClick={() => signOut()}>Sign Out</Button>
-                        ) : (
-                            <Button onClick={() => signIn()}>Sign In</Button>
-                        )}
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
+  return (
+    <div className="container mx-auto flex items-center justify-between p-4">
+      <Link href={"/"}>
+        <div className="flex items-center justify-between">
+          <Image
+            width="50"
+            height="50"
+            src="/WhatsApp Image 2023-10-15 at 15.01.46.png"
+            alt="logo"
+          />{" "}
         </div>
-    );
+      </Link>
+      <NavigationMenu>
+        <NavigationMenuList className="flex items-center">
+          <NavigationMenuItem>
+            <Link href="/admin/events/create" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                create event
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            {session ? (
+              <Button onClick={() => signOut()}>Sign Out</Button>
+            ) : (
+              <LoginModal />
+            )}
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
 }
